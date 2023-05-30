3fb5190cbbdbcea2d8bbd4dd373626db70b9de5a
Rahul Bhatnagar <RahulBhatnagar@RahulBhatnagar.onmicrosoft.com>
2023-05-30 17:48:18 -0400

    Configuring static site generation1

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,17 @@
-import SmallCard from '../components/SmallCard';
-import { projectIcons } from '../components/Icons';
+import Link from 'next/link'
 
-import { projects } from '../utils/projectsData';
+function Home() {
+    return (
+        <div>
+            <h1> Home Page </h1>
+            <Link href='/product'>
+                Product
+            </Link>
 
-const Home = () => (
-  <div className="home">
-    <h1>What Can I Deploy to Static Apps??!!!</h1>
-    <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return (
-          <SmallCard
-            key={project.id}
-            Icon={Icon}
-            title={project.name}
-            slug={project.slug}
-          />
-        );
-      })}
-    </div>
-  </div>
-);
+        </div>
+      
+      
+    ) 
+}
 
-export default Home;
- 
+export default Home
