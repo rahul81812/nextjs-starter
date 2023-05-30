bee5425c960d2ed66b8d1fdf8aea4c2753cb6129
Rahul Bhatnagar <RahulBhatnagar@RahulBhatnagar.onmicrosoft.com>
2023-05-30 17:36:07 -0400

    Configuring static site generation

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { projects } from '../utils/projectsData';
 
 const Home = () => (
   <div className="home">
-    <h1>What Can I Deploy to Static Apps??!</h1>
+    <h1>What Can I Deploy to Static Apps??!!!</h1>
     <div className="card-grid">
       {projects.map((project) => {
         const Icon = projectIcons[project.id];
@@ -23,3 +23,4 @@ const Home = () => (
 );
 
 export default Home;
+ 
