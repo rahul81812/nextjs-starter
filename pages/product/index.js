9775b91c82f124787cdf1f4de50f9b40c8e493da
rahul81812 <RahulBhatnagar@RahulBhatnagar.onmicrosoft.com>
2023-05-31 22:14:21 -0400

    serversideprops

--- pages/product/index.js
+++ pages/product/index.js
@@ -15,10 +15,10 @@ function Product({products}) {
     
     export default Product
 
-    export async function getStaticProps() {
+    export async function getServerSideProps() {
         const response = await fetch('https://prod-30.canadacentral.logic.azure.com/workflows/5075f846fa384c44850005c13c23a509/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=fJ5dTyARvk04GSxkZVkwQupGJ4k3uROadFoKS8GKbbM')
         const data = await response.json()
-        console.log(data)
+
 
         return {
             props: {
